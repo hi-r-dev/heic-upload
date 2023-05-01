351218431c82d3b33aab99da902c4ed12c64259c
hi_r_kouzu <hi.r.kouzu@gmail.com>
2023-05-01 13:23:49 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const multer = require("multer");
+
+const app = express();
+const upload = multer();
+
+app.post("/upload", upload.single("file"), (req, res) => {
+  const file = req.file;
+  if (!file) {
+    return res.status(400).send("No file uploaded");
+  }
+
+  // Save the file to disk or database
+  console.log(`Received file: ${file.originalname}`);
+  res.send("File uploaded successfully");
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
